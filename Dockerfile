7f0b9fe124d854fed5fb9acd7d649e94ee64479e
Vitor Livi <vitorlivi1999@gmail.com>
2024-12-12 09:56:15 -0300

    feat: :sparkles: add lambda document conversion logic

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM public.ecr.aws/lambda/nodejs:20-x86_64
 
 ENV PATH=/var/lang/bin:/usr/local/bin:/usr/bin/:/bin:/opt/bin
 
-# Configure linker to correctly point to libraries
 ENV LD_LIBRARY_PATH="/usr/lib:/usr/lib64"
 RUN dnf install -y xorg-x11-fonts-* libSM.x86_64 libXinerama-devel libxslt google-noto-sans-cjk-fonts binutils tar gzip xz \
   openssl nss-tools dbus-libs cups-libs && dnf clean all
@@ -13,8 +12,9 @@ RUN mkdir ~/libre && cd ~/libre && curl -s -L https://download.documentfoundatio
 RUN cd ~/libre/LibreOffice_24.8.3.2_Linux_x86-64_rpm/RPMS/ && rpm -Uvh *.rpm && rm -fr ~/libre \
   && ln -s /opt/libreoffice24.8/program/soffice.bin /usr/bin/libreoffice
 
-ENV HOME=/tmp
-
 COPY ./index.js ${LAMBDA_TASK_ROOT}/
+COPY ./package.json ${LAMBDA_TASK_ROOT}/
 
-CMD [ "handler.handler" ]
+RUN cd ${LAMBDA_TASK_ROOT} && npm install
+
+CMD [ "index.handler" ]
--- index.js
+++ index.js
@@ -1,3 +1,65 @@
+import { exec } from "child_process";
+import { writeFileSync, readFileSync, unlinkSync } from "fs";
+import { v4 as uuid } from "uuid";
+
+async function convertDocxToPdf(buffer) {
+  const tempFileName = uuid();
+  const tempDir = "/tmp";
+
+  const inputFilePath = `${tempDir}/${tempFileName}.docx`;
+  const outputFilePath = `${tempDir}/${tempFileName}.pdf`;
+
+  writeFileSync(inputFilePath, buffer);
+
+  await new Promise((resolve, reject) => {
+    const command = `libreoffice --headless --norestore --nofirststartwizard --convert-to pdf --outdir ${tempDir} ${inputFilePath}`;
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error("LibreOffice Error:", error.message);
+        reject(new Error(error.message));
+        return;
+      }
+      if (stderr) {
+        console.warn("LibreOffice Warning:", stderr);
+      }
+      console.log(stdout);
+      resolve(true);
+    });
+  }).catch((error) => {
+    throw new Error(`Error during conversion: ${error.message}`);
+  });
+
+  const pdfBuffer = readFileSync(outputFilePath);
+
+  unlinkSync(inputFilePath);
+  unlinkSync(outputFilePath);
+
+  return pdfBuffer;
+}
+
 export const handler = async (event, context) => {
-  return context.logStreamName;
+  try {
+    const { fileName, fileContent } = event;
+
+    const decodedDocxContent = Buffer.from(fileContent, "base64");
+    const pdfBuffer = await convertDocxToPdf(decodedDocxContent);
+    const encodedPdfFileContent = pdfBuffer.toString("base64");
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/octet-stream",
+        "Content-Disposition": `attachment; filename="${fileName}"`,
+      },
+      body: encodedPdfFileContent,
+      isBase64Encoded: true,
+    };
+  } catch (error) {
+    console.error("Error processing file:", error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "File processing failed." }),
+    };
+  }
 };
